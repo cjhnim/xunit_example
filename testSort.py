fc5f33507e401fc34c6df339a039c153890d6639
choi jung hyun <cjhnim@gmail.com>
2018-03-27 14:23:19 +0900

    3자리 정수 정렬 실패..

--- testSort.py
+++ testSort.py
@@ -1,25 +1,29 @@
 import unittest
 
-def mySort(num):
-    if len(num) == 1:
-        return num
-    if num[0] > num[1]:
-        tmp = num[0]
-        num[0] = num[1]
-        num[1] = tmp
-    return num
+def mySort(numbers):
+    if len(numbers) == 1:
+        return numbers
+    if len(numbers) >= 2 and numbers[0] > numbers[1]:
+        numbers[0], numbers[1] = numbers[1], numbers[0]
+    if len(numbers) >= 3 and numbers[1] > numbers[2]:
+        numbers[1], numbers[2] = numbers[2], numbers[1]
+    return numbers
 
 class test_MySort(unittest.TestCase):
-    def test_oneElements1(self):
+    def test_oneElementsZero(self):
         self.assertEquals([0],mySort([0]))
-    def test_oneElements2(self):
+    def test_oneElementsOne(self):
         self.assertEquals([1],mySort([1]))
     def test_twoElements(self):
         self.assertEquals([1,2], mySort([1,2]))
-    def test_twoElements2(self):
+    def test_twoElementsSwap(self):
         self.assertEquals([1,2], mySort([2,1]))
-
-
+    def test_threeElements1(self):
+        self.assertEquals([1,2,3], mySort([1,2,3]))
+    def test_threeElements2(self):
+        self.assertEquals([1,2,3], mySort([2,1,3]))
+    def test_threeElements_swap(self):
+        self.assertEquals([1,2,3], mySort([3,2,1]))
 
 
 
